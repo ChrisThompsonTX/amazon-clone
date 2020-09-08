3bda4fcefb89e966000bf622d0f009a90e6ca20b
Chris Thompson <chris.tx86@gmail.com>
2020-09-08 12:44:11 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcOMXXA6Aq93_7nxgtQF9y2Bzggblj54o",
+    authDomain: "clone-9bf3c.firebaseapp.com",
+    databaseURL: "https://clone-9bf3c.firebaseio.com",
+    projectId: "clone-9bf3c",
+    storageBucket: "clone-9bf3c.appspot.com",
+    messagingSenderId: "1095795370502",
+    appId: "1:1095795370502:web:21b57db08a7a8c474a7187",
+    measurementId: "G-104QZBZYBJ"
+};
