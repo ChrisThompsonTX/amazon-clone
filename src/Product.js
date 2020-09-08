a12ed716828e9e294dda86325c9313a31c31a9a8
Chris Thompson <chris.tx86@gmail.com>
2020-09-08 13:05:21 -0700

    add home page

--- src/Product.js
+++ src/Product.js
@@ -6,7 +6,22 @@ function Product() {
         <div className="product">
             <div className="product__info">
                 <p>Speaker</p>
+                <p className="product__price" >
+                    <small>$</small>
+                    <strong>12.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>🍕</p>
+                    <p>🍕</p>
+                    <p>🍕</p>
+                </div>
             </div>
+
+            <img
+                src="https://www.seriouseats.com/2017/02/20170216-detroit-style-pizza-47-1500x1125-1.jpg"
+                alt=""
+            />
+            <button>Add to Basket</button>
         </div>
     )
 }
