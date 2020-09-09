966ffd69214c810d768a8edb488db66253fef791
Chris Thompson <chris.tx86@gmail.com>
2020-09-09 10:18:50 -0700

    style subtotal componenet

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './subtotal.css'
+import React from 'react';
+import './subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 
-function Subtotal() {
+function Subtotal({ basket = [], getBasketTotal = (a) => a.length }) {
+
+
     return (
+
         <div className="subtotal">
-            subtotal
+            <CurrencyFormat 
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal ({basket.length} items):
+                            <strong>{` ${value}`}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" /> 
+                            This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
 
-export default Subtotal
+export default Subtotal;
