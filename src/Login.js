981943cdafc9408c520d3ef150421c0b2f18ce39
Chris Thompson <chris.tx86@gmail.com>
2020-09-09 12:53:17 -0700

    style login button

--- src/Login.js
+++ src/Login.js
@@ -14,10 +14,18 @@ function Login() {
             </Link>
 
             <div className="login__container">
-                <h1>Sign in</h1>
+                <h1 className="login__signin" >Sign in</h1>
                 <form>
-                    <h5></h5>
+                    <h5>E-mail</h5>
+                    <input type="text"/>
+                    <h5>Password</h5>
+                    <input type="password"/>
+                    <button className="login__signinbutton" >Sign In</button>
                 </form>
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+                <button className="login__registerbutton" >Create your Amazon Account</button>
             </div>
         </div>
     )
