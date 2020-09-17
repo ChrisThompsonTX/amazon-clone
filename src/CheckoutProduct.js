2e99f6e7324bd98f5f3ffbd62e10fa769a786662
Chris Thompson <chris.tx86@gmail.com>
2020-09-17 12:04:32 -0700

    style orders page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,7 +3,7 @@ import './checkoutproduct.css'
 import { useStateValue } from './StateProvider';
 
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
     const [{ basket }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
@@ -31,7 +31,9 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                         <p key={i}>🍕</p>
                     ))}
                 </div>
-                <button onClick={removeFromBasket}>Remove from Basket</button>
+                {!hideButton && (
+                    <button onClick={removeFromBasket}>Remove from Basket</button>
+                )}
             </div>
         </div>
     )
